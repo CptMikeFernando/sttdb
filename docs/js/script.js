6d5d2676972a5169943dcdc4b9bf93760ce658cb
CptMikeFernando <135133763+CptMikeFernando@users.noreply.github.com>
2024-05-31 12:52:35 -0500

    Update script.js

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,1 +1,19 @@
+var audio = document.getElementById('audio');
+var isPlaying = false;
 
+$('.btn-play-music').click(function(){
+    $(this).toggleClass('pause');
+    $(this).parent('.music-box').toggleClass('playing');
+    $(this).find('.fa').toggleClass('fa-pause');
+    if (isPlaying) {
+      audio.pause()
+    } else {
+      audio.play();
+    }
+});
+audio.onplaying = function() {
+  isPlaying = true;
+};
+audio.onpause = function() {
+  isPlaying = false;
+};
