cc74fd15500283baa50150b209348f37b069f07c
CptMikeFernando <135133763+CptMikeFernando@users.noreply.github.com>
2024-05-31 14:31:21 -0500

    Update script.js

--- docs/js/script.js
+++ docs/js/script.js
@@ -3,16 +3,15 @@
 const playBtn = document.querySelector(".visually-hidden");
 const label = document.querySelector(".label");
 const audio = document.querySelector(".audio");
-const video = document.querySelector(".video");
 
 playBtn.addEventListener("click", function (e) {
 	if (e.target.checked) {
-		video.play();
+		audio.play();
 		label.innerHTML = "pause";
 		label.classList.remove("move");
 		label.classList.add("back");
 	} else {
-		video.pause();
+		audio.pause();
 		label.innerHTML = "play";
 		label.classList.add("move");
 		label.classList.remove("back");
